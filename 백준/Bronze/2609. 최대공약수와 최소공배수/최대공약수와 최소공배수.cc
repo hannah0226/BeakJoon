d38fca65a69b918ea5c4abb8504dc1414fff0a1d
Lee Hannah <89291223+hannah0226@users.noreply.github.com>
2023-08-03 21:40:46 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int gcd(int a, int b) {
+	return b ? gcd(b, a % b) : a;
+}
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << gcd(a, b) << "\n" <<  a / gcd(a, b) * b;
+}
